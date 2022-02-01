7f6719bda91e87f999abdebc5b733f89d45291bd
Askar Ialiaev <a.yalyaev@kt-team.de>
2022-02-01 18:13:26 +0300

    add interactivity for slider

--- src/slider/slider.ts
+++ src/slider/slider.ts
@@ -7,12 +7,15 @@ type Options = {
   dpiWidth: number;
 };
 
+type Callback = (position: number[]) => void;
+
 export function chartSlider(
   root: HTMLElement,
   { columns, types, colors }: ChartData,
   { dpiWidth }: Options
 ) {
   const width = dpiWidth / 2;
+  const minWidth = width * 0.05;
 
   const canvas = root.querySelector('canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -20,10 +23,129 @@ export function chartSlider(
   canvas.width = dpiWidth;
   canvas.height = DPI_HEIGHT;
 
+  const $left = root.querySelector('[data-element="left"]') as HTMLElement;
+  const $window = root.querySelector('[data-element="window"]') as HTMLElement;
+  const $right = root.querySelector('[data-element="right"]') as HTMLElement;
+
   css(canvas, {
     width: `${width}px`,
     height: `${HEIGHT}px`,
   });
+
+  function getPosition() {
+    const left = parseInt($left.style.width, 10);
+    const right = width - parseInt($right.style.width, 10);
+
+    return [(left * 100) / width, (right * 100) / width];
+  }
+
+  function mousedown(e: MouseEvent) {
+    if (!(e.target instanceof HTMLElement)) {
+      return;
+    }
+
+    const dimensions = {
+      left: parseInt($window.style.left, 10),
+      right: parseInt($window.style.right, 10),
+      width: parseInt($window.style.width, 10),
+    };
+
+    const { type } = e.target.dataset;
+    const xCoord = e.pageX;
+    switch (type) {
+      case 'window': {
+        document.onmousemove = (event) => {
+          const delta = xCoord - event.pageX;
+          if (!delta) return;
+
+          const left = dimensions.left - delta;
+          const right = width - left - dimensions.width;
+
+          setPosition(left, right);
+        };
+
+        document.onmouseup = () => {
+          document.onmousemove = null;
+        };
+
+        break;
+      }
+      case 'left':
+      case 'right': {
+        document.onmousemove = (event) => {
+          const delta = xCoord - event.pageX;
+          if (!delta) return;
+
+          if (type === 'left') {
+            const left = width - (dimensions.width + delta) - dimensions.right;
+            const right = width - (dimensions.width + delta) - left;
+            setPosition(left, right);
+          } else {
+            const right = width - (dimensions.width - delta) - dimensions.left;
+            setPosition(dimensions.left, right);
+          }
+        };
+
+        document.onmouseup = () => {
+          document.onmousemove = null;
+        };
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  function setPosition(left: number, right: number) {
+    const computedWidth = width - right - left;
+
+    if (computedWidth < minWidth) {
+      css($window, {
+        width: `${minWidth}px`,
+      });
+      return;
+    }
+
+    if (left < 0) {
+      css($window, {
+        left: '0px',
+      });
+      css($left, {
+        width: '0px',
+      });
+      return;
+    }
+
+    if (right < 0) {
+      css($window, {
+        right: '0px',
+      });
+      css($right, {
+        width: '0px',
+      });
+      return;
+    }
+
+    css($window, {
+      width: `${computedWidth}px`,
+      left: `${left}px`,
+      right: `${right}px`,
+    });
+
+    css($left, {
+      width: `${left}px`,
+    });
+
+    css($right, {
+      width: `${right}px`,
+    });
+  }
+
+  root.addEventListener('mousedown', mousedown);
+
+  const defaultWidth = width * 0.3;
+  setPosition(0, defaultWidth);
 
   const yData = columns.filter(
     (column) => types[column[0] as keyof Types] === 'line'
@@ -53,4 +175,10 @@ export function chartSlider(
   mappedChartData.forEach(({ color, coords }) => {
     drawLine(ctx, coords, { lineWidth: 4, color });
   });
+
+  return {
+    subscribe(fn: Callback) {
+      fn(getPosition());
+    },
+  };
 }
